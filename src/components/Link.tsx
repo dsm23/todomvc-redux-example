731876f7b2d87d892ac6d6e827a3e36f5fc5c591
David Murdoch <dsmurdoch357@gmail.com>
2024-03-27 16:25:17 +0000

    chore(link): update link to use useAppDispatch and useAppSelector

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,22 +3,34 @@
 
 import PropTypes from "prop-types";
 import cx from "clsx";
+import { setVisibilityFilter } from "~/actions";
+import { useAppDispatch, useAppSelector } from "~/app/hooks";
 
-const Link = ({ active, children, setFilter }) => (
-  // eslint-disable jsx-a11y/anchor-is-valid
-  <a
-    className={cx({ selected: active })}
-    style={{ cursor: "pointer" }}
-    onClick={() => setFilter()}
-  >
-    {children}
-  </a>
-);
+const Link = ({ children, filter }) => {
+  const dispatch = useAppDispatch();
+  const visibilityFilter = useAppSelector((state) => state.visibilityFilter);
+
+  const active = filter === visibilityFilter;
+
+  const handleClick = () => {
+    dispatch(setVisibilityFilter(filter));
+  };
+
+  return (
+    // eslint-disable jsx-a11y/anchor-is-valid
+    <a
+      className={cx({ selected: active })}
+      style={{ cursor: "pointer" }}
+      onClick={handleClick}
+    >
+      {children}
+    </a>
+  );
+};
 
 Link.propTypes = {
-  active: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
-  setFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 export default Link;
