67dbb1da383910ac633d80725dff8e0918845830
David Murdoch <dsmurdoch357@gmail.com>
2024-03-27 01:10:26 +0000

    chore(main.tsx): switch to createRoot from render

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { render } from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./components/App";
@@ -7,9 +8,10 @@ import "todomvc-app-css/index.css";
 
 const store = createStore(reducer);
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root"),
+createRoot(document.getElementById("root") as HTMLHtmlElement).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
 );
