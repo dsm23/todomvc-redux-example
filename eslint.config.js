4ef0d5e04592791295d4759be440d5341afa5e4f
David Murdoch <dsmurdoch357@gmail.com>
2024-10-14 22:20:24 +0100

    chore(eslint): update eslint to v9 and flat config

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import * as mdx from "eslint-plugin-mdx";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
+      react.configs.flat["jsx-runtime"],
+    ],
+    files: ["**/*.{js,md,mdx,mjs,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "no-console": [
+        "error",
+        {
+          allow: ["debug", "error", "info", "trace", "warn"],
+        },
+      ],
+    },
+  },
+  {
+    // Configure.mdx
+    files: ["**/*.mdx"],
+    rules: {
+      "react/jsx-uses-vars": "error",
+    },
+  },
+  mdx.flat,
+);
