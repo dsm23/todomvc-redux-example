9b4b847479623ba56d02fc0562833a30202fb0cc
David Murdoch <dsmurdoch357@gmail.com>
2025-08-29 15:07:34 +0100

    chore(dirname): import.meta.dirname

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
 import * as mdx from "eslint-plugin-mdx";
@@ -11,9 +10,7 @@ import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
 
 export default tseslint.config(
   includeIgnoreFile(gitignorePath),
