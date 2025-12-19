00ea31fe4095a8e67c8ad0278d5f8010ef4c9ff7
David Murdoch <dsmurdoch357@gmail.com>
2025-12-19 13:05:09 +0000

    chore(deps): update dependency vite-tsconfig-paths to v6

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths({
+      projectDiscovery: "lazy",
+    }),
+  ],
 });
